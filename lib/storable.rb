20c0ee03e2dede8d7ca8f865b0d2e67501cff13d
delano <delano@solutious.com>
2010-03-23 18:16:30 -0400

    Release 0.6.5 (Yajl support)

--- lib/storable.rb
+++ lib/storable.rb
@@ -48,7 +48,7 @@ class Storable
   
   require 'storable/orderedhash' if USE_ORDERED_HASH
   unless defined?(SUPPORTED_FORMATS) # We can assume all are defined
-    VERSION = "0.6.4"
+    VERSION = "0.6.5"
     NICE_TIME_FORMAT  = "%Y-%m-%d@%H:%M:%S".freeze 
     SUPPORTED_FORMATS = [:tsv, :csv, :yaml, :json, :s, :string].freeze 
   end
