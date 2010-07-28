53323c8c19125d17398dc933533fa466fd090a21
git <delano@solutious.com>
2010-07-28 09:14:53 -0400

    Release 0.8.0 (inheritence; sensitive fields)

--- lib/storable.rb
+++ lib/storable.rb
@@ -40,7 +40,7 @@ class Storable
   require 'proc_source'  
   require 'storable/orderedhash' if USE_ORDERED_HASH
   unless defined?(SUPPORTED_FORMATS) # We can assume all are defined
-    VERSION = "0.7.4"
+    VERSION = "0.8.0"
     NICE_TIME_FORMAT  = "%Y-%m-%d@%H:%M:%S".freeze 
     SUPPORTED_FORMATS = [:tsv, :csv, :yaml, :json, :s, :string].freeze 
   end
