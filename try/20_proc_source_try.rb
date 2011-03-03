6af79678028929c92e0ff433c2b52e513be9d3b6
delano <delano@solutious.com>
2011-03-03 11:15:00 -0500

    Converting spec to tryouts (wip)

---
+++ try/20_proc_source_try.rb
@@ -0,0 +1,67 @@
+require 'proc_source'
+
+@subject = ProcString.new "{ false }"
+
+def lambda?
+  @l.call
+  return true
+end
+
+
+## string should be set
+@subject
+#=> "{ false }"
+
+## #file can be set
+@subject.file = "test.rb"
+@subject.file
+#=> "test.rb"
+  
+## #lines can be set
+@subject.lines = 1..10
+#=> (1..10)
+
+
+## should create a proc
+ps = ProcString.new "do\n false\n end"
+ps.to_proc.class
+#=> Proc
+  
+## should connect #file and #lines to the proc
+ps = ProcString.new "do\n false\n end"
+ps.file = "to_proc_test"
+ps.lines = (100..102)
+ps.to_proc.inspect.index('to_proc_test:100').nil?
+#=> false
+  
+## should fail if #file is set and #lines is a non Range
+ps = ProcString.new "do\n false\n end"
+ps.file = "to_proc_test"
+ps.lines = 100
+begin
+  ps.to_proc
+rescue RuntimeError
+  :success
+end
+#=> :success
+  
+## should create a lambda
+## Test is from: http://en.wikipedia.org/wiki/Closure_(computer_science)#Closure_leaving
+ps = ProcString.new "do\n return false\n end"
+@l = ps.to_lambda
+[@l.call, lambda?]
+#=> [false, true]
+
+## should dump and load
+@test = "do\n false\n end"
+@file = "ps_test"
+@lines = (23..25)
+p1 = ProcString.new @test
+p1.file = @file
+p1.lines = @lines
+str = Marshal.dump(p1)
+p2 = Marshal.load str
+[p2, p2.file, p2.lines]
+#=> [@test, @file, @lines]
+
+
